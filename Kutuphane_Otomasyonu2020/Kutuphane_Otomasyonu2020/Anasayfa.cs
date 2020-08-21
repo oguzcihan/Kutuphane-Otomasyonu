fdb559ccf4fb6ccd13dd38a111a017f7997441cc
oguzcihan <oguzcihan12@gmail.com>
2020-08-21 17:28:36 +0300

    first

---
+++ Kutuphane_Otomasyonu2020/Kutuphane_Otomasyonu2020/Anasayfa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kutuphane_Otomasyonu2020
+{
+    public partial class Anasayfa : Form
+    {
+        SqlConnection baglanti;
+        Connect con = new Connect();
+        public Anasayfa()
+        {
+            baglanti = new SqlConnection(con.adres);
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kutuphane_Otomasyonu2020/Kutuphane_Otomasyonu2020/connect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane_Otomasyonu2020
+{
+    class Connect
+    {
+        public string adres = System.IO.File.ReadAllText(@"\bin\Debug\Setting.txt");
+    }
+}
