1a03ed6c28c33f593e167b5eb2b1a388f4fc225d
rustemtekdemir <rustemtekdemir@gmail.com>
2020-08-22 16:00:58 +0300

    deneme

---
+++ Kutuphane_Otomasyonu2020/Kutuphane_Otomasyonu2020/kitapListesi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kutuphane_Otomasyonu2020
+{
+    public partial class kitapListesi : Form
+    {
+        public kitapListesi()
+        {
+            InitializeComponent();
+        }
+
+        private void kitapListesi_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
