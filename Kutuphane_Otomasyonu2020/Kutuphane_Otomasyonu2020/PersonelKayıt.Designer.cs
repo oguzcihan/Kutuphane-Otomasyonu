7b8c05224e5d3c703e47ea0ed10ce13b08d2fd9e
whitehunters <yusa82940@ogr.duzce.edu.tr>
2020-08-23 16:52:23 +0300

    ilk

---
+++ Kutuphane_Otomasyonu2020/Kutuphane_Otomasyonu2020/PersonelKayıt.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Kutuphane_Otomasyonu2020
+{
+    partial class PersonelKayıt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblkitapNo = new System.Windows.Forms.Label();
+            this.lblkitapAdi = new System.Windows.Forms.Label();
+            this.lblbaskiYili = new System.Windows.Forms.Label();
+            this.lblyazar = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 303);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Düzenle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(283, 303);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Sil";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // lblkitapNo
+            // 
+            this.lblkitapNo.AutoSize = true;
+            this.lblkitapNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblkitapNo.Location = new System.Drawing.Point(43, 70);
+            this.lblkitapNo.Name = "lblkitapNo";
+            this.lblkitapNo.Size = new System.Drawing.Size(90, 17);
+            this.lblkitapNo.TabIndex = 15;
+            this.lblkitapNo.Text = "Personel No:";
+            // 
+            // lblkitapAdi
+            // 
+            this.lblkitapAdi.AutoSize = true;
+            this.lblkitapAdi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblkitapAdi.Location = new System.Drawing.Point(41, 109);
+            this.lblkitapAdi.Name = "lblkitapAdi";
+            this.lblkitapAdi.Size = new System.Drawing.Size(29, 17);
+            this.lblkitapAdi.TabIndex = 16;
+            this.lblkitapAdi.Text = "Ad:";
+            // 
+            // lblbaskiYili
+            // 
+            this.lblbaskiYili.AutoSize = true;
+            this.lblbaskiYili.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblbaskiYili.Location = new System.Drawing.Point(41, 186);
+            this.lblbaskiYili.Name = "lblbaskiYili";
+            this.lblbaskiYili.Size = new System.Drawing.Size(79, 17);
+            this.lblbaskiYili.TabIndex = 17;
+            this.lblbaskiYili.Text = "Doğum Yılı:";
+            // 
+            // lblyazar
+            // 
+            this.lblyazar.AutoSize = true;
+            this.lblyazar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblyazar.Location = new System.Drawing.Point(41, 147);
+            this.lblyazar.Name = "lblyazar";
+            this.lblyazar.Size = new System.Drawing.Size(52, 17);
+            this.lblyazar.TabIndex = 18;
+            this.lblyazar.Text = "Soyad:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(135, 186);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 24);
+            this.comboBox1.TabIndex = 25;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox3.Location = new System.Drawing.Point(135, 147);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(151, 23);
+            this.textBox3.TabIndex = 24;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(135, 67);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 23);
+            this.textBox1.TabIndex = 22;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(135, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(151, 23);
+            this.textBox2.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(41, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 17);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Görevi:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(135, 227);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 24);
+            this.comboBox2.TabIndex = 27;
+            // 
+            // label14
+            // 
+            this.label14.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(613, 51);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(245, 13);
+            this.label14.TabIndex = 165;
+            this.label14.Text = "<<PERSONEL ADINA GÖRE ARAMA YAPINIZ>>";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(613, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 17);
+            this.label2.TabIndex = 164;
+            this.label2.Text = "Arama";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox7.Location = new System.Drawing.Point(668, 25);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(146, 23);
+            this.textBox7.TabIndex = 163;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(417, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(564, 439);
+            this.dataGridView1.TabIndex = 162;
+            // 
+            // PersonelKayıt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1194, 647);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.lblkitapNo);
+            this.Controls.Add(this.lblkitapAdi);
+            this.Controls.Add(this.lblbaskiYili);
+            this.Controls.Add(this.lblyazar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "PersonelKayıt";
+            this.Text = "PersonelKayıt";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblkitapNo;
+        private System.Windows.Forms.Label lblkitapAdi;
+        private System.Windows.Forms.Label lblbaskiYili;
+        private System.Windows.Forms.Label lblyazar;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Kutuphane_Otomasyonu2020/Kutuphane_Otomasyonu2020/PersonelKayıt.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kutuphane_Otomasyonu2020
+{
+    public partial class PersonelKayıt : Form
+    {
+        public PersonelKayıt()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public abstract class personel
+        {
+            public int personelno;
+            public string personelAd;
+            public string personelSoyad;
+            public string Dogumyili;
+            public abstract void gorevi();
+
+            
+        }
+        class admin:personel
+        {
+            public override void gorevi()
+            {
+                throw new NotImplementedException();
+            }
+            public Boolean yetki = true;
+        }
+
+        
+            public void ekle()
+            {
+            admin ali = new admin();
+            ali.personelAd = textBox1.Text;
+            }
+        
+    }
+}
