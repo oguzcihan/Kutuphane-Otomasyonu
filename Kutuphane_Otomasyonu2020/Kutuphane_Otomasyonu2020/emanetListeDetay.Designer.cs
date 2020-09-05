58c1cea81a6e43284f9ca5c89b17a2bf6e08257b
oguzcihan <oguzcihan12@gmail.com>
2020-09-05 21:39:38 +0300

    listeler

--- Kutuphane_Otomasyonu2020/Kutuphane_Otomasyonu2020/emanetListeDetay.Designer.cs
+++ Kutuphane_Otomasyonu2020/Kutuphane_Otomasyonu2020/emanetListeDetay.Designer.cs
@@ -83,6 +83,7 @@
             this.button2.TabIndex = 79;
             this.button2.Text = "Geri Dön";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // buttonKitapTeslimAlindi
             // 
